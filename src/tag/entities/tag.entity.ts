b53a7ff618f8684c0c844c32ce36fed9e585cf44
Qaisar Irfan <qirfan@jobcase.com>
2022-11-11 17:15:37 +0500

    Tags and migration

---
+++ src/tag/entities/tag.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'tags' })
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
---
+++ src/tag/tag.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from './entities/tag.entity';
+
+@Injectable()
+export class TagService {
+  constructor(
+    @InjectRepository(Tag) private readonly tagReopsitory: Repository<Tag>,
+  ) {}
+  create(createTagDto: CreateTagDto) {
+    return 'This action adds a new tag';
+  }
+
+  async findAll(): Promise<Tag[]> {
+    return await this.tagReopsitory.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} tag`;
+  }
+
+  update(id: number, updateTagDto: UpdateTagDto) {
+    return `This action updates a #${id} tag`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} tag`;
+  }
+}
